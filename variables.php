cbf784c487046cc09e6d6e803f08f33582342876
Reptilius <blockgen@yandex.ru>
2023-12-20 00:05:08 +0300

    page_120

--- variables.php
+++ variables.php
@@ -1,6 +1,30 @@
 
 <?
-$tiket_price = 3000;
-$vip_tiket = $tiket_price + 500;
 
-echo "Price of tiket is: " . $tiket_price;
+use function PHPSTORM_META\type;
+
+$ticket_price = 3000;
+$vip_ticket = $ticket_price + 500;
+$a = $b = $c = 711;
+$phrase = 'Hello, Alabama!';
+
+$days = [];
+$days += [85];
+
+
+echo "Price of tiket is: $ticket_price \r\n<br />";
+echo "Price for VIP is: $vip_ticket \n<br />";
+echo "$ticket_price\n";
+$a = 999;
+echo "$a\r\n$b\n$c";
+// unset($phrase);
+echo $phrase."\n<br />";
+echo "$ticket_price\r\n<br />";
+echo $days[0];
+echo PHP_FLOAT_MAX."<br />";
+echo is_int($a);
+
+$str_1 = '';
+if (isset($str_1)){
+    echo "It's Alive!";
+}
